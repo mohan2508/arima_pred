240ec8dc0a8850813fe575128d05e5865cfcafe8
mohan2508 <115917715+mohan2508@users.noreply.github.com>
2024-12-04 22:17:56 +0530

    Update arima_pred.py

--- arima_pred.py
+++ arima_pred.py
@@ -8,99 +8,55 @@ from datetime import date
 from statsmodels.tsa.arima.model import ARIMA
 import statsmodels.api as sm
 
-
-
-st.markdown('## Stock :orange[Web] :red[App]')
+st.header('Stock :orange[Web] :red[App]')
 st.image("https://th.bing.com/th/id/OIP.Ttoy04OYis0BLYxlPZvTSwHaEK?w=331&h=186&c=7&r=0&o=5&dpr=1.3&pid=1.7")
-st.markdown('## Stock :red[Future] :green[Prediction]')
-
+st.subheader(':rainbow[Stock Future Prediction]')
 stocks=st.text_input('Enter Stock Ticker','SBIN.NS')
 Start='2015-01-01'
 Today = date.today().strftime("%Y-%m-%d")
 
+button1=st.button("Load Data")
+
+if button1:
+    data=yf.Ticker(stocks)
+    hys_data=data.history(period='10y')
+    hys_data.reset_index(inplace=True)
+    st.write(hys_data.tail())
+    
+st.subheader(':orange[Prrediction] :red[of the] :green[Stock Selected]') 
 data=yf.Ticker(stocks)
-hys_data=data.history(period='10y')
-hys_data.reset_index(inplace=True)
+hys_data=data.history(period='10y')  
 
+opt = st.radio(':rainbow[Market Status :]',['Open','High','Low','Close'],horizontal=True) 
 
-
-data,prediction1,prediction2=st.tabs(['Data','Prediction1','Prediction2'])
-
-with data:
-    st.subheader(":orange[Raw] :green[Data]")
-    st.write(hys_data.tail(10))
-
-with prediction1:
-     open,high,low,close=st.tabs(['Open','High','Low','Close'])
-
-     with open:
-         df1=hys_data.reset_index()['Open']
-         model=ARIMA(df1,order=(2,1,3))
-         result=model.fit()
-         forecast_steps=5
-         forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
-         st.write(forecast_values)
-     
-     with high:
-         df1=hys_data.reset_index()['High']
-         model=ARIMA(df1,order=(2,1,3))
-         result=model.fit()
-         forecast_steps=5
-         forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
-         st.write(forecast_values)
-         
-     with low:
-         df1=hys_data.reset_index()['Low']
-         model=ARIMA(df1,order=(2,1,3))
-         result=model.fit()
-         forecast_steps=5
-         forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
-         st.write(forecast_values) 
-         
-     with close:
-         df1=hys_data.reset_index()['Close']
-         model=ARIMA(df1,order=(2,1,3))
-         result=model.fit()
-         forecast_steps=5
-         forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
-         st.write(forecast_values)
-         
-order=(1,1,1)
-seasonal_order=(1,1,1,12)   
-model2=sm.tsa.SARIMAX(df1,order=order,seasonal_order=seasonal_order)   
-result=model2.fit() 
-forecast_steps=5   
-forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)   
-with prediction2:
-     open,high,low,close=st.tabs(['Open','High','Low','Close'])
-
-     with open:
-        df1=hys_data.reset_index()['High']
-        model2=sm.tsa.SARIMAX(df1,order=order,seasonal_order=seasonal_order)   
-        result=model2.fit() 
-        forecast_steps=5    
-        forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)          
-        st.write(forecast_values)
-     
-     with high:
-        df1=hys_data.reset_index()['High']
-        model2=sm.tsa.SARIMAX(df1,order=order,seasonal_order=seasonal_order)   
-        result=model2.fit() 
-        forecast_steps=5    
-        forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)          
-        st.write(forecast_values)
-     with low:
-        df1=hys_data.reset_index()['Low']
-        model2=sm.tsa.SARIMAX(df1,order=order,seasonal_order=seasonal_order)   
-        result=model2.fit() 
-        forecast_steps=5    
-        forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)          
-        st.write(forecast_values) 
-            
-     with close:
-        df1=hys_data.reset_index()['Close']
-        model2=sm.tsa.SARIMAX(df1,order=order,seasonal_order=seasonal_order)   
-        result=model2.fit() 
-        forecast_steps=5    
-        forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)          
-        st.write(forecast_values)
+if opt=='Open':
+    df1=hys_data.reset_index()['Open']
+    model=ARIMA(df1,order=(2,1,3))
+    result=model.fit()
+    forecast_steps=5
+    forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
+    st.write(forecast_values)
+    
+elif opt=='High':
+    df1=hys_data.reset_index()['High']
+    model=ARIMA(df1,order=(2,1,3))
+    result=model.fit()
+    forecast_steps=5
+    forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
+    st.write(forecast_values) 
+    
+elif opt=='Low':
+    df1=hys_data.reset_index()['Low']
+    model=ARIMA(df1,order=(2,1,3))
+    result=model.fit()
+    forecast_steps=5
+    forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
+    st.write(forecast_values) 
+    
+elif opt=='Close':
+    df1=hys_data.reset_index()['Close']
+    model=ARIMA(df1,order=(2,1,3))
+    result=model.fit()
+    forecast_steps=5
+    forecast_values=result.predict(start=len(df1),end=len(df1)+forecast_steps-1,dynamic=False)
+    st.write(forecast_values)     
